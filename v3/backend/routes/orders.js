406c810ad3a176c0424a71222ba8413996539792
rohitarrunachalam <rrarohit@gmail.com>
2025-01-13 18:13:52 +0530

    Cleaned up code

--- v3/backend/routes/orders.js
+++ v3/backend/routes/orders.js
@@ -1,25 +1,3 @@
-// // routes/orders.js
-// const express = require('express');
-// const router = express.Router();
-// const { createOrder, updateOrder, getOrder, getAllOrders } = require('../controllers/orders');
-// const {authenticateToken} = require('../middleware/auth');
-// const {validateOrder} = require('../middleware/validation');
-
-// // Create order
-// router.post('/', authenticateToken, createOrder);
-
-// // Update order
-// router.put('/:id', authenticateToken, updateOrder);
-
-// // Get order details
-// router.get('/:id', authenticateToken, getOrder);
-
-// // Get all orders
-// router.get('/', authenticateToken, getAllOrders);
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const { createOrder, updateOrder, getOrder, getAllOrders } = require('../controllers/orders');
